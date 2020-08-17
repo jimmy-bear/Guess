491a12d1f54a18959642a2685fc1a82797ed52ac
Jimmy <jtmmy3715@gmail.com>
2020-08-17 18:59:56 +0800

    驚爆密碼基礎版

--- app/src/main/java/com/example/guess/SecretGame.kt
+++ app/src/main/java/com/example/guess/SecretGame.kt
@@ -2,33 +2,54 @@ package com.example.guess
 
 import java.util.*
 
+
+const val MINVALUE=0
+const val MAXVALUE=100
 class SecretGame (){
     init {
         resetGame()
     }
-    var secret=Random().nextInt(10)+1
+
+    var secret=0
     var time=0
     var isEndGame=false
-    fun diff(number:Int):Int{
-        time++
-        if (number==secret) {
-            isEndGame = true
-        }
-        return number-secret
-    }
-    fun getMessage(diff:Int):String{
-        return if (diff > 0) {
-            "Guess Smailler"
-        } else if (diff < 0) {
-            "Guess Bigger"
+    var minValue=MINVALUE
+    var maxValue=MAXVALUE
+    val ERRORNUMBER= MAXVALUE+1
+
+    fun diff(number: Int):Int{
+        //make sure input 1-100
+        if(number<=maxValue && number>=minValue) {
+            time++
+
+            if (number == secret) {
+                isEndGame = true
+            }
+            return number-secret
+
         }else {
-            "You got it"
+            return ERRORNUMBER
         }
 
+
+    }
+    fun getMessage(dif:Int):String{
+
+        return if (dif > 0 && dif<maxValue) {
+            "Guess Smailler"
+        } else if (dif < 0) {
+            "Guess Bigger"
+        }else if(dif==0){
+            "You got it"
+        }else{
+            "ErrorNumber"
+        }
     }
     fun resetGame(){
-        secret= Random().nextInt(10)+1
+        secret= Random().nextInt(MAXVALUE)+1
         time=0
+        minValue=MINVALUE
+        maxValue=MAXVALUE
         isEndGame=false
     }
 
